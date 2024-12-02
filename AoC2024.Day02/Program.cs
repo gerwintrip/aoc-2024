6f4aa5b4eac3f98b886a4f1260bbdc1fa3062a93
Gerwin Trip <gerwintrip@gmail.com>
2024-12-02 13:58:24 +0100

    Day02

---
+++ AoC2024.Day02/Program.cs
@@ -0,0 +1,34 @@
+﻿using AoC2024.Utils;
+
+var lines = FileUtils.ReadInputFile().ToList();
+
+var list = lines.Select(line => line.Split(" ").Select(int.Parse).ToList()).ToList();
+
+var count = list.Count(CheckNumbers);
+
+Console.WriteLine("Part 1: " + count);
+
+var count2 = list.Count(numbers =>
+{
+    for (var i = 0; i < numbers.Count; i++)
+    {
+        var copy = numbers.ToList();
+        copy.RemoveAt(i);
+        if (CheckNumbers(copy)) return true;
+    }
+    return false;
+});
+
+Console.WriteLine("Part 2: " + count2);
+return;
+
+bool CheckNumbers(List<int> numbers)
+{
+    var increasing = numbers[0] < numbers[1];
+    for (var i = 0; i < numbers.Count - 1; i++)
+    {
+        if (Math.Abs(numbers[i] - numbers[i + 1]) > 3 || numbers[i] == numbers[i + 1]) return false;
+        if ((increasing && numbers[i] > numbers[i + 1]) || (!increasing && numbers[i] < numbers[i + 1])) return false;
+    }
+    return true;
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent of Code 2024
 
-Day 1 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day01/Program.cs)
+Day 1 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day01/Program.cs) <br>
+Day 2 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day02/Program.cs)
