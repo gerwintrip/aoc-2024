98ddb011553be4c76865b7a6e4c2031d17d83a30
Gerwin Trip <gerwintrip@gmail.com>
2024-12-03 11:57:21 +0100

    Day03

---
+++ AoC2024.Day03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using AoC2024.Utils;
+
+var lines = FileUtils.ReadInputFile().ToList();
+
+var sum = lines.Select(line => MulRegex().Matches(line).Sum(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value))).Sum();
+
+Console.WriteLine("Part 1: " + sum);
+
+var entireString = lines.Aggregate((a, b) => a + b);
+var sum2 = 0;
+var enabled = true;
+var tokens = SplitRegex().Split(entireString);
+
+foreach (var token in tokens)
+{
+    if (token == "do()") enabled = true;
+    else if (token == "don't()") enabled = false;
+    else if (token.StartsWith("mul(") && enabled)
+    {
+        var match = MulRegex().Match(token);
+        if (match.Success) sum2 += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+    }
+}
+Console.WriteLine("Part 2: " + sum2);
+
+
+internal partial class Program
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex MulRegex();
+    
+    [GeneratedRegex(@"(mul\(\d+,\d+\)|do\(\)|don't\(\))")]
+    private static partial Regex SplitRegex();
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Advent of Code 2024
 
 Day 1 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day01/Program.cs) <br>
-Day 2 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day02/Program.cs)
+Day 2 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day02/Program.cs) <br>
+Day 3 Solution: [Click here](https://github.com/gerwintrip/aoc-2024/blob/master/AoC2024.Day03/Program.cs)
